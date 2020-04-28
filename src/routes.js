951f1e4a50fcf8424959f437fc30638083b65c85
croschel <croschel000@gmail.com>
2020-04-28 15:48:32 -0300

    User Controller

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,11 @@ routes.post('/auth', SessionController.store);
 // After this middleware all route must have token
 routes.use(authMiddleware);
 
+// Users Routes
 routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
+routes.delete('/users/:id', UserController.delete);
+routes.get('/users', UserController.index);
+
 
 export default routes;
